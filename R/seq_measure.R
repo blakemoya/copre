a031f1710ce64e22a31dbdcfa761fd7a1b56bfbc
blakemoya <blakemoya@utexas.edu>
2022-11-11 13:29:52 -0600

    Implemented Pitman-Yor sequence measure

--- R/seq_measure.R
+++ R/seq_measure.R
@@ -7,14 +7,12 @@
 #' @param Po A function on a sequence length \code{n}, a number of unique values
 #'  \code{k}, and the number of values equal to j \code{kj} that returns the
 #'  probability of the next member in the sequence having the value j.
-#' @param rnext An optional optimized generating function. If \code{NULL}, one
-#'  will be generate from \code{Pn} and \code{Po}.
 #'
 #' @return A \code{seq_measure} object for use in the exchangeable sequence
 #'  resampling scheme for mixtures.
-#' @seealso {[exseqre()]}
+#' @seealso {[seqre()]}
 #' @export
-seq_measure <- function(pars, Pn, Po, rnext = NULL) {
+seq_measure <- function(pars, Pn, Po) {
   obj <- list(pars = pars, Pn = Pn, Po = Po)
   pnext <- function(z) {
     if (is.null(z) | !is.numeric(z)) {
@@ -31,40 +29,6 @@ seq_measure <- function(pars, Pn, Po, rnext = NULL) {
     probs <- c(sapply(tab, function(kj) Po(n, k, kj)), Pn(n, k))
     names(probs) <- c(vals, min(setdiff(1:(n + 1), vals)))
     return(probs)
-  }
-  if (is.null(rnext)) {
-    rnext <- function(n, z = numeric(0)) {
-      n0 <- length(z)
-      zz <- numeric(n)
-      for (nn in 1:n) {
-        probs <- pnext(c(z, zz[0:(nn - 1)]))
-        zz[nn] <- sample(as.numeric(names(probs)), 1, prob = probs)
-      }
-      return(zz)
-    }
-  }
-
-  class(obj) <- 'seq_measure'
-  obj$pnext <- pnext
-  obj$rnext <- rnext
-  return(obj)
-}
-
-#' A Collapsed Gnedin Process Sequence Measure.
-#'
-#' @param gamma The gamma parameter for the Gnedin process with xi set to 0.
-#'  Bounded to \code{[0, 1]}.
-#'
-#' @return A \code{seq_measure} object for use in the exchangeable sequence
-#'  resampling scheme for mixtures.
-#' @seealso {[seq_measure(), exseqre()]}
-#' @export
-Sq_gnedin0 <- function(gamma) {
-  Pn <- function(n, k) {
-    k * (k - gamma) / (n * (gamma + n))
-  }
-  Po <- function(n, k, kj) {
-    (gamma + n - k) / (n * (gamma + n)) * (kj + 1)
   }
   rnext <- function(n, z = numeric(0)) {
     n0 <- length(z)
@@ -96,5 +60,58 @@ Sq_gnedin0 <- function(gamma) {
     }
     return(zz)
   }
-  return(seq_measure(gamma, Pn, Po, rnext))
+
+  class(obj) <- 'seq_measure'
+  obj$pnext <- pnext
+  obj$rnext <- rnext
+  return(obj)
+}
+
+#' A Pitman-Yor Sequence Measure.
+#'
+#' @param sigma The discount parameter for the Pitman-Yor process. Must be less
+#'  than 1.
+#' @param theta The concentration parameter for the Pitman-Yor process. Must be
+#'  greater than -\code{sigma} if \code{sigma} is in [0, 1), else ignored.
+#' @param m A positive integer used to set \code{theta = m * abs(sigma)} if
+#'  \code{sigma} is negative.
+#'
+#' @return A \code{seq_measure} object for use in the exchangeable sequence
+#'  resampling scheme for mixtures.
+#' @seealso [seq_measure()], [seqre()]
+#' @export
+Sq_pitmanyor <- function(sigma, theta = 1, m = 1L) {
+  if (sigma < 0) {
+    if (m < 0 | !is.integer(m)) {
+      stop('`m` must be a positive integer. Remember to add `L` after the
+           numeric value.')
+    }
+    theta <- m * abs(sigma)
+  }
+  Pn <- function(n, k) {
+    (theta + sigma * k) / (theta + n)
+  }
+  Po <- function(n, k, kj) {
+    (kj - sigma)  / (theta + n)
+  }
+  return(seq_measure(gamma, Pn, Po))
+}
+
+#' A Collapsed Gnedin Process Sequence Measure.
+#'
+#' @param gamma The gamma parameter for the Gnedin process with xi set to 0.
+#'  Bounded to \code{[0, 1]}.
+#'
+#' @return A \code{seq_measure} object for use in the exchangeable sequence
+#'  resampling scheme for mixtures.
+#' @seealso {[seq_measure(), exseqre()]}
+#' @export
+Sq_gnedin0 <- function(gamma) {
+  Pn <- function(n, k) {
+    k * (k - gamma) / (n * (gamma + n))
+  }
+  Po <- function(n, k, kj) {
+    (gamma + n - k) / (n * (gamma + n)) * (kj + 1)
+  }
+  return(seq_measure(gamma, Pn, Po))
 }
