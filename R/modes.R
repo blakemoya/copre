7d3e079a064d1be6eeb2fb41dd6bdbaeb701988a
blakemoya <blakemoya@utexas.edu>
2023-01-28 00:33:57 -0600

    Add index return option to `modes`, added alias `antimodes`

--- R/modes.R
+++ R/modes.R
@@ -6,25 +6,48 @@
 #' @param obj A `copre_result` or `mdp_result` object.
 #' @param mean A logical value indicating whether to extract the modes of the
 #'  mean density of each of the individual sampled density.
-#' @param grd For `mdpolya_result`, a grid on which to evaluate the object.
+#' @param grd For a `seqre_result`, a grid on which to evaluate the object.
+#' @param idx A logical value indicating whether to also return the index within
+#'  `grd` of the discovered modes.
 #' @param anti A logical value indicating whether to extract true modes or
 #'  anti-modes.
 #'
 #' @return A matrix of modes values in the support of the `copre_result` density
 #' @export
-modes <- function(obj, mean = TRUE, grd = NULL, anti = FALSE) {
+modes <- function(obj, mean = FALSE, grd = NULL, idx = FALSE, anti = FALSE) {
   UseMethod('modes')
+}
+
+#' Antiode Extractor
+#'
+#' @description Extracts the antimodes from a `copre_result` or `seqre_result`
+#'  object.
+#'
+#' @param obj A `copre_result` or `mdp_result` object.
+#' @param mean A logical value indicating whether to extract the modes of the
+#'  mean density of each of the individual sampled density.
+#' @param grd For `mdpolya_result`, a grid on which to evaluate the object.
+#' @param idx A logical value indicating whether to also return the index within
+#'  `grd` of the discovered modes.
+#'
+#' @return A matrix of antimodes values in the support of the `copre_result`
+#'  density
+#' @export
+antimodes <- function(obj, mean = FALSE, grd = NULL, idx = FALSE) {
+  modes(obj, mean = mean, grd = grd, idx = idx, anti = TRUE)
 }
 
 #' @describeIn modes Mode-counting method for `seqre_result` objects.
 #' @export
-modes.seqre_result <- function(obj, mean = TRUE, grd = NULL, anti = FALSE) {
+modes.seqre_result <- function(obj, mean = FALSE, grd = NULL, idx = FALSE,
+                               anti = FALSE) {
   modes(grideval(obj, grd = grd, func = 'density'), mean = mean, anti = anti)
 }
 
 #' @describeIn modes Mode-counting method for `grideval_result` objects.
 #' @export
-modes.grideval_result <- function(obj, mean = TRUE, grd = NULL, anti = FALSE) {
+modes.grideval_result <- function(obj, mean = FALSE, grd = NULL, idx = FALSE,
+                                  anti = FALSE) {
   if (obj$func != 'density') {
     if ('copre_result' %in% class(obj)) {
       obj <- grideval(obj, func = 'density')
@@ -33,7 +56,11 @@ modes.grideval_result <- function(obj, mean = TRUE, grd = NULL, anti = FALSE) {
     }
   }
   if (anti) {
-    f <- function(p) obj$grid[pracma::findpeaks(-p)[, 2]]
+    obj <- -obj
+  }
+  if (idx) {
+    f <- function(p) list(idx = pracma::findpeaks(p)[, 2],
+                          value = obj$grid[pracma::findpeaks(p)[, 2]])
   } else {
     f <- function(p) obj$grid[pracma::findpeaks(p)[, 2]]
   }
@@ -54,7 +81,7 @@ modes.grideval_result <- function(obj, mean = TRUE, grd = NULL, anti = FALSE) {
 #' @describeIn modes Counts the modes from a `copre_result` or `seqre_result`
 #'   object.
 #' @export
-n_modes <- function(obj, mean = TRUE, grd = NULL, anti = FALSE) {
+n_modes <- function(obj, mean = FALSE, grd = NULL, anti = FALSE) {
   if (mean) {
     return(length(modes(obj, mean = TRUE, grd = grd, anti = anti)))
   } else {
