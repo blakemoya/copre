839e1d23e7b0d163925084ccf5b9212a560f1c16
blakemoya <blakemoya@utexas.edu>
2023-01-28 00:34:54 -0600

    Added additional arguments option to `functional`

--- R/functional.R
+++ R/functional.R
@@ -2,13 +2,15 @@
 #'
 #' @param obj A `copre_result` object.
 #' @param f A list of functions.
+#' @param ... Additional arguments passed to `f`.
 #' @param mean A logical value indicating whether or not to obtain the
-#'  functional from the pointwise mean of the sampled distributions or from each individually.
+#'  functional from the pointwise mean of the sampled distributions or from each
+#'  individually.
 #'
 #' @return The integral over the `copre_result` grid of the functions in the
 #'   list multiplied by the density of each sample distribution in `obj`.
 #' @export
-functional <- function(obj, f, mean = FALSE) {
+functional <- function(obj, f, ..., mean = FALSE) {
   if (obj$func != 'density') {
     if ('copre_result' %in% class(obj)) {
       obj <- grideval(obj, func = 'density')
@@ -20,6 +22,6 @@ functional <- function(obj, f, mean = FALSE) {
     obj <- matrix(apply(obj, 1, mean), ncol = 1)
   }
   out <- apply(obj, 1, function(y)
-      pracma::trapz(obj$grid, f(obj$grid) * y))
+      pracma::trapz(obj$grid, f(obj$grid, ...) * y))
   return(out)
 }
